b236f08f92e40894193d7e13a5e911c4536c1a00
zqzkt <xiaoqing.ng.2023@smu.edu.sg>
2025-05-19 18:39:12 +0800

    Set up account registration

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
-export default function Home() {
+export default function Home(){
     return(
-        <div>Home</div>
-    );
+        <div>
+        
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+        </div>
+    )
 }
--- pages/Register.jsx
+++ pages/Register.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import supabase from "../helper/supabaseClient";
+import { TextField, Box } from "@mui/material";
+import Button from "@mui/material/Button";
 
 export default function Register() {
-    return(
-        <div>Register</div>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setMessage("");
+
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+
+    console.log(data, error)
+
+    if (error) {
+      setMessage(error.message);
+      setLoading(false);
+      return;
+    }
+
+    if (data) {
+      setMessage("Account successfully created!");
+      setEmail("");
+      setPassword("");
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <Box
+        component="form"
+        sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="Email"
+          value={email}
+          required
+        />
+
+        <TextField
+          onChange={(e) => setPassword(e.target.value)}
+          type="text"
+          placeholder="Password"
+          value={password}
+          required
+        />
+        <br></br>
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ backgroundColor: "black" }}
+          size="large"
+          onClick={() => setLoading(true)}
+          loading={loading}
+        >
+          Register
+        </Button>
+      </Box>
+
+      {message && <span>{message}</span>}
+    </div>
+  );
 }
