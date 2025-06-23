ce9acc8f70acf891002ecc0300d7a3130ff79e21
zqzkt <xiaoqing.ng.2023@smu.edu.sg>
2025-06-23 18:18:55 +0800

    Naming changes

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import supabase from "../helper/supabaseClient";
 import Button from "@mui/material/Button";
 import { Box } from "@mui/material";
-import InProgressCoursesSummary from "../src/components/InProgressCoursesSummary";
+import InProgressCourses from "../src/components/InProgressCourses";
 import AllCoursesSummary from "../src/components/AllCoursesSummary";
 import NavigationBar from "../src/components/NavigationBar";
 
@@ -14,7 +14,7 @@ export default function Dashboard() {
       <div>
         {/* <h1>Welcome to Accelerate!</h1> */}
         <Box>
-          <InProgressCoursesSummary />
+          <InProgressCourses />
           <AllCoursesSummary />
         </Box>
       </div>
